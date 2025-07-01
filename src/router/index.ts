219b7cbe47c68d1e2b95b56223fb4efba4fbd0dd
Dominick Muniz <dominick.muniz7@gmail.com>
2025-07-01 06:14:26 -0600

    font update.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
-import HomePageKeyframes from '@/views/HomePageKeyframes.vue'
+import HomePageKeyframes from '@/views/HomePageKeyFrames.vue'
 
 const routes = [
   {
